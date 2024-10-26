f508d8598e40429d97d33d055ed8d714b0f1e7cc
DevByXenon <santinob.dev@gmail.com>
2024-10-26 15:37:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ var Libro = /** @class */ (function () {
     return Libro;
 }());
 // Creando instancias de la clase Libro
-var libro1 = new Libro("Cien años de soledad", "Gabriel García Márquez", 1967);
+var libro2 = new Libro("Cien años de soledad", "Gabriel García Márquez", 1967);
 // [Tu código para el segundo libro]
-var libro2 = new Libro("El principito", "Antoine de Saint-Exupéry", 1943);
+var libro1 = new Libro("El principito", "Antoine de Saint-Exupéry", 1943);
 // Usando el método getAntiguedad y mostrando detalles de los libros
 console.log("El libro \"".concat(libro1.titulo, "\" de ").concat(libro1.autor, ", publicado en ").concat(libro1.añoPublicacion, ", tiene ").concat(libro1.getAntiguedad(), " a\u00F1os de antig\u00FCedad."));
 // [Tu código para imprimir los datos del segundo libro]
